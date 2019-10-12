70ea5004a982d9451e8258e05f01332cbd5aea05
Everaldo-Gomes <everaldo332@gmail.com>
2019-10-12 15:52:59 -0300

    update

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 class Interpretador{
+
+    private Variavel[] var = new Variavel[3000];
+    private int linhaAtual;
+
+    public int getLinhaAtual(){return this.linhaAtual;}
     
     public void comecaInterpretar(String comandos[]){
-	int a = 1;
+	//
     }
-    
-    
-   
-
 }
+
