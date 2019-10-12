0f8433b189ca3bd176a926a003f3f72404656ab1
Everaldo-Gomes <everaldo332@gmail.com>
2019-10-12 15:52:26 -0300

    cada linha vai pra uma posição

--- fonte/EG.java
+++ fonte/EG.java
@@ -9,22 +9,29 @@ class EG{
 	File arquivo = new File(args[0]); //args[0] pega o nome "main.eg" passdo no terminal
 	Interpretador objInterpretador = new Interpretador(); //cria o objeto que vai ser o arquivo
 	String comandos[] = new String[3000];
-    	
+
 	try{
 	    Scanner sc = new Scanner(arquivo);
 	    
 	    //guarda cada linha em cada posição
-	    int posicao = 0;
+	    int a = 0;
 	    while(sc.hasNextLine()){
-		//quebra quando tiver um espaço em branco, e pode quebrar em até 3000 entradas.
-		String getLine = sc.nextLine(); //a cada espaço, pega o que foi digitado e inseri no array comandos
-		comandos = getLine.split(" ",3000);
+		comandos[a] = sc.nextLine();
+		a++;
 	    }
+
+	    //se quiser ver o que tem no array "comandos" executa a linha abaixo
+	    //int f = comandos.length;
+	    for(int i = 0; i < 20; i++)
+		System.out.println(comandos[i]);	
+	    
+	    
 	    objInterpretador.comecaInterpretar(comandos);//começa a interpretar 
 	    sc.close(); //termina de ler o arquivo
 	} 
 	catch (FileNotFoundException e){
 	    System.out.println("Não foi possível executar " + args[0] + "." + " Verifique o nome do arquivo.");
 	}
+	
     }
 }
