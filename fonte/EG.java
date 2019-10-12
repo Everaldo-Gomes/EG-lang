a7fe73e56f4fb54120b512fcbfff618b3b456014
Everaldo-Gomes <everaldo332@gmail.com>
2019-10-12 11:23:37 -0300

    nova forma de ler arquivos

--- fonte/EG.java
+++ fonte/EG.java
@@ -5,16 +5,21 @@ import java.io.FileNotFoundException;
 
 class EG{ 
     public static void main(String args[]) throws Exception{
-        
-	Interpretador objInterpretador = new Interpretador(); //cria o objeto que vai ser o arquivo
 	
 	File arquivo = new File(args[0]); //args[0] pega o nome "main.eg" passdo no terminal
+	Interpretador objInterpretador = new Interpretador(); //cria o objeto que vai ser o arquivo
+	String comandos[] = new String[3000];
+    	
 	try{
 	    Scanner sc = new Scanner(arquivo);
-	    while(sc.hasNextLine()){            //começa a ler o arquivo
-		String getLine = sc.nextLine(); //a cada espaço, pega o que foi digitado e inseri no array comandos
-		objInterpretador.setComando(getLine);//chama a função pra guardar os comandos em cada posição
+	    
+	    //guarda cada linha em cada posição
+	    int posicao = 0;
+	    while(sc.hasNextLine()){            
+		comandos[posicao] = sc.nextLine();
+		posicao++;
 	    }
+	    objInterpretador.comecaInterpretar(comandos);
 	    sc.close(); //termina de ler o arquivo
 	} 
 	catch (FileNotFoundException e){
