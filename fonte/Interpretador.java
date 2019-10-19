606bdf37de21e90df7fb4186d5784335c10736d2
Everaldo-Gomes <everaldo332@gmail.com>
2019-10-19 20:15:58 -0300

    update

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Interpretador{
     
     private Variavel[] var = new Variavel[3000];   //usada para guardar as variáveis
-    private Variavel a = new Variavel();
+    private Variavel objVariavel = new Variavel();
     private Operador operadorObj = new Operador(); //usada para decidir qual operador vai usar
     private int linhaAtual;
     
@@ -13,22 +13,21 @@ class Interpretador{
 	this.linhaAtual = 1;
 	int posicao = 0; //posicao do vetor de variaveis
 
-	//NÃO TENHO CERTEZA SE ESTÁ DO JEITO CORRETO
 	for(int i = 0; i < totalLinha; i++,this.linhaAtual++){
 	    //cria variáveis
-	    if(comandos[i].equals(a.getCria())){
-		String nome  = comandos[i+1];
-		double valor = Double.parseDouble(comandos[i+2]);
-		var[posicao] = new Variavel(nome, valor);
+	    if(comandos[i].equals(objVariavel.getCria())){
+		var[posicao] = new Variavel(comandos[i+1]);//,Double.parseDouble(comandos[i+2]));
+		//var[posicao].setNome(comandos[i+1]);
+		//var[posicao].setValor(Double.parseDouble(comandos[i+2]));//setValor recebe o parametro valor que antes é convertido em doulbe
 		posicao++;
 	    }
 
-	    /*
+	    
 	    //calcula as operações básicas
 	    else if(comandos[i].equals(operadorObj.getSoma())){
 		double soma = var[posicao].getValor() + var[posicao-1].getValor();
 		System.out.println(soma);
-	    }
+	    }/*
 	    else if(comandos[i].equals(operadorObj.getSubtracao())){
 		double subtracao = var[posicao-1].getValor() - var[posicao-2].getValor();
 	    }
@@ -50,7 +49,7 @@ class Interpretador{
 	    */
 	}
 	for(int i = 0; i < 3; i++){
-	    System.out.println(var[i].getNome() + " " + var[i].getValor());
+	    //System.out.println(var[i].getNome() + " " + var[i].getValor());
 	}
     }
 }
