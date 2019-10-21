c825db15c5c6c361d51625e6aeab69012a1cb276
Everaldo-Gomes <everaldo332@gmail.com>
2019-10-20 21:14:14 -0300

    erro corrigido

--- fonte/Variavel.java
+++ fonte/Variavel.java
@@ -1,11 +1,11 @@
-xclass Variavel{
+class Variavel{
 
     public  static final String cria = "Cria";
     private String nome;
     private double valor;
 
-    public void   setNome (String n){this.nome = n;    }
-    public void   setValor(double v){this.valor = v;   }
+    public void setNome   (String n){this.nome = n;    }
+    public void setValor  (double v){this.valor = v;   }
     public String getNome ()        {return this.nome; }
     public double getValor()        {return this.valor;}
 
